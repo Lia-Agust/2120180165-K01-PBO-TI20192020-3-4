7a0754439ec4065ae15543a8009330e510e6b87b
Lia Agustina <55940309+Lia-Agust@users.noreply.github.com>
2019-10-25 20:16:04 +0700

    Upload Kasus 01

---
+++ 2120180165-K01-PBO-TI20192020-3-4-02/Biodata.java
@@ -0,0 +1,61 @@
+
+package latihan1;
+
+/**
+ *
+ * @author LIA AGUSTINA
+ */
+public class Latihan1 {
+
+    public static void main(String[] args) {
+        // instansiasi dari class mahasiswa
+        System.out.println("BIODATA KELAS");
+        System.out.println("mahasiswa 1");
+        mahasiswa mhs_satu = new mahasiswa ();
+        mhs_satu.nama ="Lia Agustina";//akses biodata
+        mhs_satu.alamat ="Dander";//akses biodata
+        mhs_satu.nim ="2120180165";//akses biodata
+                System.out.println("Nama ="+mhs_satu.nama);
+                System.out.println("Alamat ="+mhs_satu.alamat);
+                System.out.println("NIM ="+mhs_satu.nim);
+                
+        System.out.println("mahasiswa 2");
+        mahasiswa mhs_dua = new mahasiswa ();
+        mhs_dua.nama ="Indah Purnama Sari";//akses biodata
+        mhs_dua.alamat ="Dander";//akses biodata
+        mhs_dua.nim ="2120180158";//akses biodata
+                System.out.println("Nama ="+mhs_dua.nama);
+                System.out.println("Alamat ="+mhs_dua.alamat);
+                System.out.println("NIM ="+mhs_dua.nim);
+                
+        System.out.println("mahasiswa 3");
+         mahasiswa mhs_tiga = new mahasiswa ();
+        mhs_tiga.nama ="Hidayatis Sifyani";//akses biodata
+        mhs_tiga.alamat ="Kalitidu";//akses biodata
+        mhs_tiga.nim ="2120180155";//akses biodata
+                System.out.println("Nama ="+mhs_tiga.nama);
+                System.out.println("Alamat ="+mhs_tiga.alamat);
+                System.out.println("NIM ="+mhs_tiga.nim);
+                
+        System.out.println("mahasiswa 4");
+        mahasiswa mhs_empat = new mahasiswa ();
+        mhs_empat.nama ="Agus Setia Rega";//akses biodata
+        mhs_empat.alamat ="Ngasem";//akses biodata
+        mhs_empat.nim ="2120180132";//akses biodata
+                System.out.println("Nama ="+mhs_empat.nama);
+                System.out.println("Alamat ="+mhs_empat.alamat);
+                System.out.println("NIM ="+mhs_empat.nim);
+               
+        System.out.println("mahasiswa 5");
+        mahasiswa mhs_lima = new mahasiswa ();
+        mhs_lima.nama ="Deddy Gilang Riski Muharom";//akses biodata
+        mhs_lima.alamat ="Padangan";//akses biodata
+        mhs_lima.nim ="2120180143";//akses biodata
+                System.out.println("Nama ="+mhs_lima.nama);
+                System.out.println("Alamat ="+mhs_lima.alamat);
+                System.out.println("NIM ="+mhs_lima.nim);
+                
+        
+    }
+    
+}
---
+++ 2120180165-K01-PBO-TI20192020-3-4-02/class Mahasiswa.java
@@ -0,0 +1,25 @@
+
+package latihan1;
+
+/**
+ *
+ * @author LIA AGUSTINA
+ */
+public class mahasiswa {
+    //memiliki : biodata
+    String nama;
+    String alamat;
+    String nim;
+    int biodata;
+    //fungsi :method
+    void biodata (){
+        System.out.println("nama");
+        System.out.println("alamat");
+        System.out.println("nim");
+    }
+
+        
+        
+    }
+    
+
